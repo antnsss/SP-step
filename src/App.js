0bce9f34edf90339fd28e3c5f4e48d1888b84af7
antnsss <your-email@example.com>
2025-05-03 14:14:47 +0300

    create single page step page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const messages = [
+    'Learn React ⚛️',
+    'Apply for jobs 💼',
+    'Invest your new income 🤑',
+  ];
+
+  const [step, setStep] = React.useState(1)
+
+  const handlePrevious = () => {
+    if (step > 1) return  setStep(step - 1)
+  }
+
+  const handleNext = () => {
+    if (step < 3) return  setStep(step + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? 'active' : ''} `}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''} `}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''} `}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step-1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
